ea8a8c94d77f7ba634cc010e8e2d359f4f27ff02
dhristovva <dstanoichevax@gmail.com>
2024-04-15 15:16:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+# task 1
+print ("Hello,world!")
+
+# task 2
+name = "Julia"
+age = 23
+favourite_color = "pink"
+print("My name is", name, "I am", age, "years old, and my favorite color is", favourite_color)
+print('-' * 20)
+
+
+#task 3
+lenght = 5
+width = 3
+area = lenght * width
+print("The area of the rectangle is :", area)
+
+#task 4
+Fahrenheit = 54
+Celsius = ((Fahrenheit - 32) * 5)/ 9
+print("Temperature in Celsius is:")
+print (Celsius)
+
+#task 5
+num1= float(input("Enter the first number"))
+num2 = float(input("Enter the second number"))
+sum_result= num1 + num2
+print ("Sum", sum_result)
+product_result = num1 * num2
+print("Product" , product_result)
+difference_result = num1 - num2
+print ("Difference" , difference_result)
+quotient_result = num1 / num2
+print ("Quotient" , quotient_result)
+
+
+#task 6
+
+
+#task 7
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is even.")
+else:
+    print(num, "is odd.")
+print('-' * 20)
+
+#task 8
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+print('-' * 20)
+
+
+#task 9
+user_input = input("Enter a string")
+length= len(user_input)
+print("Length of the string:", length)
+
+#task 10
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+concatenated_string = string1 + string2
+print("Concatenated string:", concatenated_string)
+
+
