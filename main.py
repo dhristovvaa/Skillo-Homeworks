7c4cc81e21458a14313692e90ad8a1cc634238e6
Julia Stanoicheva <166553201+dhristovvaa@users.noreply.github.com>
2024-07-18 00:17:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,33 @@
-# task 1
-print ("Hello,world!")
-
-# task 2
-name = "Julia"
-age = 23
-favourite_color = "pink"
-print("My name is", name, "I am", age, "years old, and my favorite color is", favourite_color)
-print('-' * 20)
 
 
-#task 3
-lenght = 5
-width = 3
-area = lenght * width
-print("The area of the rectangle is :", area)
+from users import User
+from admins import Admin
+from movies import Movie, MOVIE_NAMES
 
-#task 4
-Fahrenheit = 54
-Celsius = ((Fahrenheit - 32) * 5)/ 9
-print("Temperature in Celsius is:")
-print (Celsius)
+# Create movie instances
+movies_library = [Movie(title, "Genre", 0, "Release Date") for title in MOVIE_NAMES]
 
-#task 5
-num1= float(input("Enter the first number"))
-num2 = float(input("Enter the second number"))
-sum_result= num1 + num2
-print ("Sum", sum_result)
-product_result = num1 * num2
-print("Product" , product_result)
-difference_result = num1 - num2
-print ("Difference" , difference_result)
-quotient_result = num1 / num2
-print ("Quotient" , quotient_result)
+# Create a user
+user1 = User("user1", "user1@example.com", "password", "premium")
 
+# Add movies to favorites
+for movie in movies_library:
+    user1.add_to_favorites(movie)
 
-#task 6
+# Get recommendations
+recommendations = user1.get_recommendations()
+print("Recommended movies:")
+for movie in recommendations:
+    print(movie.title)
 
+# Mark movies as watched and rate them
+for movie in recommendations:
+    user1.mark_as_watched(movie)
+    user1.rate_movie(movie, 4)
 
-#task 7
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print(num, "is even.")
-else:
-    print(num, "is odd.")
-print('-' * 20)
-
-#task 8
-age = int(input("Enter your age: "))
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
-print('-' * 20)
-
-
-#task 9
-user_input = input("Enter a string")
-length= len(user_input)
-print("Length of the string:", length)
-
-#task 10
-string1 = input("Enter the first string: ")
-string2 = input("Enter the second string: ")
-concatenated_string = string1 + string2
-print("Concatenated string:", concatenated_string)
-
+# Get watched list
+watched_list = user1.get_watched_list()
+print("\nWatched movies:")
+for movie in watched_list:
+    print(movie.title, "- Rating:", movie.rating)
 
